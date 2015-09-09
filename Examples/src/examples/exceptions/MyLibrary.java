6a437215986eee9a640ea1d4854b43da39edff51
cbt33 <cbt33@users.noreply.github.com>
2015-09-09 14:40:05 -0400

    Update MyLibrary.java

--- Examples/src/examples/exceptions/MyLibrary.java
+++ Examples/src/examples/exceptions/MyLibrary.java
@@ -22,5 +22,34 @@ public class MyLibrary {
 		double tip = b * p;
 		return tip;
 	}
+	
+	public static long Fac(int number) throws NotAPositiveIntegerException {
+		if (number < 0)
+			throw new NotAPositiveIntegerException(); 
+		if (number==0)
+			return 1;
+		else
+			return Fac(number-1)*number;
+	}
 
+	public static int locate(double value, double[] scores) throws ArrayException{
+		if (scores == null)
+			return new ArrayException();
+		int i = 0;
+		for (double score : scores) {
+			if (score == value)
+				return i;
+			i++;
+		}
+		return -1;
+	}
+	
+	public integer getInteger() {
+		Scanner input = new Scanner(System.input);
+		boolean isInteger = false;
+		while (!isInteger) {
+			System.out.println("Please enter an integer value");
+		}
+	}
+	
 }
