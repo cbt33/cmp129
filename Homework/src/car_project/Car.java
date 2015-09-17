9654097489618d493bcc11e23c2e3145e9ddb200
cbt33 <cbt33@users.noreply.github.com>
2015-09-17 10:12:12 -0400

    Update Car.java

--- Homework/src/car_project/Car.java
+++ Homework/src/car_project/Car.java
@@ -2,5 +2,39 @@ package car_project;
 
 public class Car {
 	private int year;
-	private BigDecimal 
+	private price;
+	
+	public Car() {
+		year = 1970;
+		price = 0.0;
+	}
+	
+	public Car(int year, double price) {
+		try {
+			this.setYear(year);
+			this.setPrice(price);
+		} catch (Exception e) {}
+	}
+	
+	public void setYear(int year) {
+		if (year < 1970 || year > 2011)
+			throw new Exception();
+		else
+			this.year = year;
+	}
+	
+	public void setPrice(double price) {
+		if (price < 0.0 || price > 10000.0)
+			throw new Exception();
+		else
+			this.price = price;
+	}
+	
+	public int year getYear() {
+		return this.year;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
 }
