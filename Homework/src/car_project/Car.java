02ac223155a0e69a9d17674512f4233485299b45
cbt33 <cbt33@users.noreply.github.com>
2015-09-18 13:12:10 -0400

    Update Car.java

--- Homework/src/car_project/Car.java
+++ Homework/src/car_project/Car.java
@@ -3,35 +3,62 @@ package car_project;
 public class Car {
 	private int year;
 	private price;
-	private static int carCount;
+	private static int carCount = 0;
+	private String make;
+	private String vin;
 	
 	public Car() {
 		year = 1970;
 		price = 0.0;
+		make = "N/A";
+		vin = "N/A";
+		carCount++;
 	}
 	
 	public Car(int year, double price) {
 		try {
 			this.setYear(year);
 			this.setPrice(price);
-		} catch (Exception e) {}
+			carCount++;
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+	
+	public Car(int year, double price, String make, String vin) {
+		try {
+			this.setYear(year);
+			this.setPrice(price);
+			this.setMake(make);
+			this.setVin(vin);
+			carCount++;
+		} catch (Exception e) {
+			e.getMessage();
+		}
 	}
 	
 	public Car(Car car) {
 		this.setYear(car.getYear());
 		this.setPrice(car.getPrice());
+		this.setMake(car.getMake());
+		this.setVin(car.getVin());
+		carCount++;
 	}
 	
-	public void setYear(int year) {
+	public String toString() {
+		return "The current car's stats are [Year:" + this.getYear()", Price:" + this.getPrice() + "]";
+	}
+	
+	public void setYear(int year) throws CarException {
 		if (year < 1970 || year > 2011)
-			throw new Exception();
+			throw new CarException();
 		else
 			this.year = year;
 	}
 	
-	public void setPrice(double price) {
+	public void setPrice(double price) throws CarException {
 		if (price < 0.0 || price > 10000.0)
-			throw new Exception();
+			throw new CarException();
 		else
 			this.price = price;
 	}
@@ -42,5 +69,13 @@ public class Car {
 	
 	public double getPrice() {
 		return this.price;
+	}
+	
+	public int getCarCount() {
+		return carCount;
+	}
+	
+	public void finalize(){
+		System.out.println("The finalize method called.");
 	}
 }
