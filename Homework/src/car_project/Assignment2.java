6a1b70b9a6d8606411f05070e1207f6b142e1fe4
cbt33 <cbt33@users.noreply.github.com>
2015-10-01 10:10:59 -0400

    Create Assignment2.java

---
+++ Homework/src/car_project/Assignment2.java
@@ -0,0 +1,23 @@
+public class Assignment2 {
+
+  public static carConstructorTest() {}
+
+  public static void carSetYearTest() {
+    Car test = new Car();
+    //Below lower Limit
+    //At lower limit
+    //Valid input
+    //At upper limit
+    //Above upper limit
+    //Negative
+    //Zero
+  }
+  public static void carSetPriceTest() {}
+  public static void carGetYearTest() {}
+  public static void carGetPriceTest() {}
+
+  public static void main(String[] args) {
+    Car test = new Car();
+    
+  }
+}
