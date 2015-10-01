fce8190afe0e5098f8b8e75c0641ace61dd5401d
cbt33 <cbt33@users.noreply.github.com>
2015-10-01 12:58:01 -0400

    Update Assignment2.java

--- Homework/src/car_project/Assignment2.java
+++ Homework/src/car_project/Assignment2.java
@@ -5,19 +5,37 @@ public class Assignment2 {
   public static void carSetYearTest() {
     Car test = new Car();
     //Below lower Limit
+    test.setYear(1969);
     //At lower limit
+    test.setYear(1970);
     //Valid input
+    test.setYear(1971);
     //At upper limit
+    test.setYear(2011);
     //Above upper limit
+    test.setYear(2012);
     //Negative
+    test.setYear(-1);
     //Zero
+    test.setYear(0);
   }
-  public static void carSetPriceTest() {}
-  public static void carGetYearTest() {}
-  public static void carGetPriceTest() {}
+  
+  public static void carSetPriceTest() {
+    Car test = new Car();
+    //Below lower Limit
+    test.setPrice(-1);
+    //At lower limit
+    test.setPrice(0);
+    //Valid input
+    test.setPrice(1);
+    //At upper limit
+    test.setPrice(100000);
+    //Above upper limit
+    test.setPrice(100001);
+  }
 
   public static void main(String[] args) {
-    Car test = new Car();
-    
+    carSetYearTest();
+    carSetPriceTest();
   }
 }
