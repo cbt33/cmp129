b9d27ce229402187b2ea7e68712dd100f2a6bd4a
cbt33 <cowboytisme@gmail.com>
2015-10-02 23:21:32 -0400

    Fixed test files, added Assignment6 tests

---
+++ Homework/src/car_project/Assignment6DecCarCountTest.java
@@ -0,0 +1,24 @@
+package car_project;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Assignment6DecCarCountTest {
+	public void tempCars() throws CarException {
+		Car car1 = new Car();
+		Car car2 = new Car();
+	}
+	
+	@Test
+	public void decCarCountTest() throws CarException, InterruptedException {
+		Car c1 = new Car();
+		Car c2 = new Car();
+		Car c3 = new Car();
+		tempCars();
+		System.runFinalization();
+		System.gc();
+		Thread.sleep(5000);
+		assertEquals(3, c1.getCarCount());
+	}
+}
