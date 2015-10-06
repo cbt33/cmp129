3b5cc0d8b357d78370e91ad82d511d5eb6474283
cbt33 <cbt33@users.noreply.github.com>
2015-10-06 13:09:19 -0400

    Update Assignment7.java

--- Homework/src/fraction/Assignment7.java
+++ Homework/src/fraction/Assignment7.java
@@ -35,11 +35,57 @@ public class Assignment7 {
 	}
 	
 	@Test
-	public void simplifyTest() throws FractionException {
+	public void simplifyTest1() throws FractionException {
 		Fraction improper = new Fraction(24, 32);
 		improper.simplify();
-		System.out.println(improper.getNumerator());
-		assertEquals(improper, new Fraction(3,4));
+		assertEquals(true, improper.toString().equals("[+3/4]"));
+	}
+	
+	@Test
+	public void simplifyTest2() throws FractionException {
+		Fraction improper = new Fraction(15, 3);
+		improper.simplify();
+		assertEquals(true, improper.toString().equals("[+5/1]"));
+	}
+	
+	@Test
+	public void copyConstructorTest() throws FractionException {
+		Fraction f = new Fraction(15, 17);
+		Fraction r = new Fraction( f );
+		assertEquals(true, f.toString().equals(r.toString()));
+	}
+	
+	@Test
+	public void cloneTest() throws FractionException, CloneNotSupportedException {
+		Fraction orig = new Fraction(13, 8);
+		Fraction c = (Fraction)orig.clone();
+		assertEquals(orig.getNumerator(), c.getNumerator());
+		assertEquals(orig.getDenominator(), c.getDenominator());
+	}
+	
+	@Test
+	public void oppositeTest() throws FractionException {
+		Fraction w = new Fraction(13, 8);
+		Fraction w2 = w.opposite();
+		assertEquals(true, w2.toString().equals("[-13/8]"));
+		assertEquals(true, w.toString().equals("[+13/8]"));
+	}
+	
+	@Test
+	public void inverseTest() throws FractionException {
+		Fraction w = new Fraction(4, -9);
+		Fraction w2 = w.inverse();
+		assertEquals(true, w2.toString().equals("[-9/4]"));
+		assertEquals(true, w.toString().equals("[-4/9]"));
+	}
+	
+	@Test
+	public void addTest() throws FractionException {
+		Fraction  k = new Fraction( 1 , 7 );
+		Fraction  r  = new Fraction( 4 , 5 );
+		r.add( k );
+		assertEquals(true, r.toString().equals("[+33/35]"));
+		assertEquals(true, k.toString().equals("[+1/7]"));
 	}
 
 }
