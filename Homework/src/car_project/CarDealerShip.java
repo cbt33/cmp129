f5d3e987275962589555b59f11315d4c51de29ec
cbt33 <cbt33@users.noreply.github.com>
2015-10-07 13:07:47 -0400

    Update CarDealerShip.java

--- Homework/src/car_project/CarDealerShip.java
+++ Homework/src/car_project/CarDealerShip.java
@@ -1,5 +1,23 @@
 package car_project;
 
 public class CarDealerShip {
-	Car[] car;
+	private Car[] cars;
+	private int numCars;
+	
+	//Default constructor, generates object from formatted text file
+	public CarDealerShip(String filename) {}
+	
+	//Adds car to cars
+	public void Add(Car car) {}
+	
+	//Delete the last car added from cars
+	public boolean Delete() {}
+	
+	//Outputs each car in cars
+	public String toString() {}
+	
+	//Writes cars into a text file before closing
+	@Override
+	protected void finalize() {}
+	
 }
