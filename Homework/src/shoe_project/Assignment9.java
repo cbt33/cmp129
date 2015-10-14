87496c38ca1030b5ab32fa9a8e1523cab42fa0fe
cbt33 <cbt33@users.noreply.github.com>
2015-10-14 16:40:41 -0400

    Update Assignment9.java

--- Homework/src/shoe_project/Assignment9.java
+++ Homework/src/shoe_project/Assignment9.java
@@ -5,8 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Assignment9 {
-	@Test
-	public void ShoeTest() {
-		assertEquals(true,true);
+	
+	//If Fulll
+	@Test (expected == ShoeException.class)
+	public void shoeInventoryAddTest() throws ShoeException {
+		ShoeInventory si = new ShoeInventory(3);
+		si.add(new ShoeType("id", 2.0, 1, 1));
+		si.add(new ShoeType("id", 3.0, 1, 1));
+		si.add(new ShoeType("id", 4.0, 1, 1));
+		si.add(new ShoeType("id", 4.0, 1, 1));
+		//assertEquals(true,true);
 	}
+	
+	@Test (expected == ShoeException.class)
+	public void shoeInventoryAddTest() throws ShoeException {
+		ShoeInventory si = new ShoeInventory(3);
+		si.add(new ShoeType("id", 2.0, 1, 1));
+		si.add(new ShoeType("id", 3.0, 1, 1));
+		si.add(new ShoeType("id", 4.0, 1, 1));
+		si.add(new ShoeType("id", 4.0, 1, 1));
+		//assertEquals(true,true);
+	}
+	
 }
